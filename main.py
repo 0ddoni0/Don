9bcd752aa5c69e9680e2153ca8e1d8a4ee9b473d
Da_Vinchi <igselyak@gmail.com>
2021-09-17 16:35:36 +0300

    update 3.0

--- main.py
+++ main.py
@@ -11,17 +11,38 @@ def getIndex():
     return render_template('index.html')
 
 
-@app.route("/signup")
-def signup():
+@app.route("/signup", methods=['GET'])
+def signupGet():
     return render_template('signup.html')
+
+
+@app.route("/signup", methods=['POST'])
+def signupPost():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    repeatpass = request.form.get('repeatpass')
+    if password != repeatpass:
+        return render_template('signup.html', message='Passwords not match')
+    else:
+        return render_template('signup.html', message='Hello, '+str(email))
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    print(request.form.get('email'))
+    print(request.form.get('password'))
+    return render_template('index.html')
+
 
 @app.route('/static/<path:path>')
 def sendFile(path):
     return send_from_directory('static', path)
 
+
 @app.route('/about')
 def getabout():
     return render_template('about.html')
+
 
 @app.route('/news')
 def getnews():
